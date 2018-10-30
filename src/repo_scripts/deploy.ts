6e04d7b4adb4b3fd6205cebdfee481e07a226f10
Mate Pek <mate.pek@gmail.com>
2018-10-30 16:58:52 +0100

    new publish script

--- src/repo_scripts/deploy.ts
+++ src/repo_scripts/deploy.ts
@@ -5,22 +5,50 @@
 // https://stackoverflow.com/questions/51925941/travis-ci-how-to-push-to-master-branch
 // https://stackoverflow.com/questions/23277391/how-to-publish-to-github-pages-from-travis-ci
 
+// TODO
+/*
+ez igy nagyon nem jo
+kell csinalni egy kommitot [skip travis] taggel
+ezt meg kell tagelni
+ehhez kell github releases apit hasznalva publisholni, travis relelases cucc
+mehet a kukaba, not good enough sign-elni kell publusholni kell mindezt csak
+masteren
+*/
+
 import * as assert from 'assert';
 import * as cp from 'child_process';
 import * as fs from 'fs';
+import * as path from 'path';
+import * as request from 'request-promise';
 import {inspect, promisify} from 'util';
+import * as vsce from 'vsce';
+
+
+const repoId = 'matepek-vscode-catch2-test-adapter';
+
 
 try {
-  main(process.argv.slice(2))
+  process.env['GITHUB_API_KEY'] = 'a395a14cca36c15c2e62064895a3bb8d06720547';
+  createGithubRelease({version: '2.0.0'}, 'tmp');
+  main;
+  // main(process.argv.slice(2))
 } catch (e) {
   console.log(inspect(e));
   process.exit(1);
 }
 
 async function main(argv: string[]) {
-  const version = await updateChangelog();
-  await updatePackageJson(version);
-  await gitCommitAndTag(version);
+  assert.strictEqual(
+      path.basename(path.dirname(process.cwd())), 'vscode-catch2-test-adapter');
+  assert.strictEqual(process.env['TRAVIS_BRANCH'], 'master');
+
+  const info = await updateChangelog();
+  await updatePackageJson(info);
+  await gitCommitAndTag(info);
+  const packagePath = await createPackage(info);
+  await gitPush();
+  await createGithubRelease(info, packagePath);
+  await publishPackage(packagePath);
 }
 
 ///
@@ -72,7 +100,7 @@ async function updateChangelog() {
   };
 }
 
-async function updatePackageJson(version: {[prop: string]: string|undefined}) {
+async function updatePackageJson(info: {[prop: string]: string|undefined}) {
   console.log('Parsing package.json');
   const packageJsonBuffer = await promisify(fs.readFile)('package.json');
 
@@ -91,15 +119,15 @@ async function updatePackageJson(version: {[prop: string]: string|undefined}) {
   assert.notStrictEqual(match[3], undefined);
 
   const packageJsonWithVer =
-      packageJson.substr(0, match.index! + match[1].length) + version.version +
+      packageJson.substr(0, match.index! + match[1].length) + info.version +
       packageJson.substr(match.index! + match[1].length + match[2].length);
 
   console.log('Updating package.json');
   await promisify(fs.writeFile)('package.json', packageJsonWithVer);
 }
 
-async function gitCommitAndTag(version: {[prop: string]: string|undefined}) {
-  console.log('Creating signed tag and pushing to origin');
+async function gitCommitAndTag(info: {[prop: string]: string|undefined}) {
+  console.log('Creating commit and signed tag');
 
   await spawn(
       'git', 'config', '--local', 'user.name',
@@ -113,23 +141,95 @@ async function gitCommitAndTag(version: {[prop: string]: string|undefined}) {
 
   await spawn('git', 'status');
   assert.ok(process.env['TRAVIS_BRANCH'] != undefined);
-  const branch = process.env['TRAVIS_BRANCH']!;
-  await spawn('git', 'checkout', branch);
-  await spawn('git', 'add', '--', 'CHANGELOG.md', 'package.json');
+  await spawn(
+      'git', 'add', '--', 'CHANGELOG.md', 'package.json', 'package-lock.json');
   await spawn('git', 'status');
+  // [skip travis-ci]: because we dont want to build the new commit it again
   await spawn(
       'git', 'commit', '-m',
-      '[Updated] Release info in CHANGELOG.md: ' + version.full +
+      '[Updated] Release info in CHANGELOG.md: ' + info.full +
           ' [skip travis-ci]');
 
-  const tagName = 'v' + version.version;
-  await spawn('git', 'tag', '-a', tagName, '-m', 'Version v' + version.version);
+  // const logOutput = cp.execSync('git log -n 1', {encoding: 'utf8'});
+  // const match = logOutput.match(/^commit ([^ ]+)/);
+  // assert.ok(match != null);
+  // assert.strictEqual(match!.length, 2);
+  // info.commitHash = match![1];
+
+  const tagName = 'v' + info.version;
+  await spawn('git', 'tag', '-a', tagName, '-m', 'Version v' + info.version);
+}
+
+async function gitPush() {
+  console.log('Pushing to origin');
+
+  const branch = process.env['TRAVIS_BRANCH']!;
   assert.ok(process.env['GITHUB_API_KEY'] != undefined);
+  await spawn('git', 'checkout', branch);
   await spawn(
       'git', 'push', '--follow-tags',
       'https://matepek:' + process.env['GITHUB_API_KEY']! +
-          '@github.com/matepek/vscode-catch2-test-adapter.git',
-      branch + ':' + branch);
+          '@github.com/matepek/vscode-catch2-test-adapter.git');
+}
+
+async function createPackage(version: {[prop: string]: string|undefined}) {
+  console.log('Creating vsce package');
+  const packagePath = './out/' + repoId + '-' + version.version + '.vsix';
+  await vsce.createVSIX({'cwd': '.', 'packagePath': packagePath});
+  return packagePath;
+}
+
+async function publishPackage(packagePath: string) {
+  console.log('Publishing vsce package');
+  assert.ok(process.env['VSCE_PAT'] != undefined);
+  // TODO
+  // await vsce.publishVSIX(packagePath, {'pat': process.env['VSCE_PAT']!});
+}
+
+async function createGithubRelease(
+    info: {[prop: string]: string|undefined}, packagePath: string) {
+  console.log('Publishing to github releases');
+  assert.ok(process.env['GITHUB_API_KEY'] != undefined);
+  const key = process.env['GITHUB_API_KEY']!;
+
+  const latestRes = JSON.parse(
+      (await request
+           .get({
+             url:
+                 'https://api.github.com/repos/matepek/vscode-catch2-test-adapter/releases/latest',
+             headers: {'User-Agent': 'matepek'}
+           })
+           .auth('matepek', key))
+          .toString());
+  assert.notStrictEqual(latestRes.tag_name, 'v' + info.version);
+
+  const createReleaseRes = JSON.parse(
+      (await request
+           .post({
+             url:
+                 'https://api.github.com/repos/matepek/vscode-catch2-test-adapter/releases',
+             headers: {'User-Agent': 'matepek'}
+           })
+           .auth('matepek', key)
+           .json(JSON.stringify({
+             'tag_name': 'testtag'  //'v' + info.version,
+           })))
+          .toString());
+
+  const uploadAssetRequest =
+      request
+          .post({
+            url: createReleaseRes.upload_url.replace(
+                '{?name,label}',
+                '?name=vscode-catch2-test-adapter-' + info.version + '.vsix'),
+            headers:
+                {'User-Agent': 'matepek', 'Content-Type': 'application/zip'}
+          })
+          .auth('matepek', key);
+
+  fs.createReadStream(packagePath).pipe(uploadAssetRequest);
+
+  uploadAssetRequest;
 }
 
 async function spawn(command: string, ...args: string[]) {
