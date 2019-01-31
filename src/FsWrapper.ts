9d4c65af54eeb9bc24d2be3cd1cde510c1595c2a
Mate Pek <mate.pek@gmail.com>
2019-01-31 19:18:47 +0100

    FsWrapper.spawnAsync error handling fix
    Why do we have this function? Because like this mocking the tests are more simple

--- src/FsWrapper.ts
+++ src/FsWrapper.ts
@@ -6,7 +6,7 @@ import * as cp from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 
-export interface SpawnReturns extends cp.SpawnSyncReturns<string> { };
+export interface SpawnReturns extends cp.SpawnSyncReturns<string> { closed: boolean };
 export interface SpawnOptions extends cp.SpawnOptions { };
 
 export function spawnAsync(
@@ -21,32 +21,46 @@ export function spawnAsync(
       status: 0,
       signal: <string><unknown>null,
       error: <Error><unknown>undefined,
+      closed: false,
     };
 
     const command = cp.spawn(cmd, args, options);
 
     ret.pid = command.pid;
+
     command.stdout.on('data', function (data) {
       ret.stdout += data;
       ret.output[1] = ret.stdout;
     });
+
     command.stderr.on('data', function (data) {
       ret.stderr += data;
       ret.output[2] = ret.stderr;
     });
+
     command.on('error', function (err: Error) {
       ret.error = err;
       reject(err);
     });
-    command.on('close', function (code) {
-      ret.status = code;
-      resolve(ret)
+
+    command.on('close', function (code: number, signal: string) {
+      ret.closed = true;
+
+      if (signal !== null) {
+        ret.signal = signal;
+        reject(new Error('FsWrapper.spawnAsync signal: ' + signal));
+      } else {
+        ret.status = code;
+        resolve(ret);
+      }
     });
 
     if (timeout !== undefined && timeout > 0) {
       setTimeout(() => {
-        command.kill('SIGKILL');
-        reject(new Error('FsWrapper.spawnAsync timeout: ' + timeout));
+        if (ret.closed !== true) {
+          command.kill('SIGKILL');
+          reject(new Error('FsWrapper.spawnAsync timeout: ' + timeout));
+        }
       }, timeout);
     }
   })
