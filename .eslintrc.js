7b90dd681118a7893d1f6d2af58244e7998d329a
Mate Pek <mate.pek@gmail.com>
2019-02-18 09:35:34 +0100

    eslint is forced by tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,5 @@ module.exports = {
 
         "@typescript-eslint/no-explicit-any": "off",
         "prettier/prettier":'warn',
-    }
+    },
 };
---
+++ src/test/TestESLint.test.ts
@@ -0,0 +1,28 @@
+//-----------------------------------------------------------------------------
+// vscode-catch2-test-adapter was written by Mate Pek, and is placed in the
+// public domain. The author hereby disclaims copyright to this source code.
+
+import * as path from 'path';
+import * as lint from 'mocha-eslint';
+
+describe(path.basename(__filename), function() {
+  lint('src/*.ts', {
+    // Specify style of output
+    formatter: 'compact', // Defaults to `stylish`
+
+    // Only display warnings if a test is failing
+    alwaysWarn: false, // Defaults to `true`, always show warnings
+
+    // Increase the timeout of the test if linting takes to long
+    timeout: 10000, // Defaults to the global mocha `timeout` option
+
+    // Increase the time until a test is marked as slow
+    slow: 4000, // Defaults to the global mocha `slow` option
+
+    // Consider linting warnings as errors and return failure
+    strict: true, // Defaults to `false`, only notify the warnings
+
+    // Specify the mocha context in which to run tests
+    contextName: 'eslint src/*.ts', // Defaults to `eslint`, but can be any string
+  });
+});
