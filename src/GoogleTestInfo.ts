ee554db2f64642bb5c7f0ac34ba676069ee105a8
Mate Pek <mate.pek@gmail.com>
2019-10-30 00:54:12 +0700

    debug enabled tests too

--- src/GoogleTestInfo.ts
+++ src/GoogleTestInfo.ts
@@ -44,6 +44,7 @@ export class GoogleTestInfo extends AbstractTestInfo {
   public getDebugParams(breakOnFailure: boolean): string[] {
     const debugParams: string[] = ['--gtest_color=no', '--gtest_filter=' + this.testNameAsId];
     if (breakOnFailure) debugParams.push('--gtest_break_on_failure');
+    debugParams.push('--gtest_also_run_disabled_tests');
     return debugParams;
   }
 
