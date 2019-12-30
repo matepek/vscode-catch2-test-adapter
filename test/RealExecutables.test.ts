fd9f93f244423818d022f3e5a9accf39108f16e6
Mate Pek <mate.pek@agoda.com>
2019-12-30 11:15:01 +0700

    test adjustment

--- test/RealExecutables.test.ts
+++ test/RealExecutables.test.ts
@@ -130,7 +130,7 @@ describe(path.basename(__filename), function() {
 
       const eventCount = adapter.testStatesEvents.length;
       await adapter.run([adapter.root.id]);
-      assert.strictEqual(adapter.testStatesEvents.length - eventCount, 90, inspect(adapter.testStatesEvents));
+      assert.strictEqual(adapter.testStatesEvents.length - eventCount, 92, inspect(adapter.testStatesEvents));
     });
 
     it.skip('should be notified by watcher', async function() {
