6065ba861a62ee220b02ea10b23e92d6750b057d
Mate Pek <mate.pek@gmail.com>
2020-04-16 12:47:14 +0700

    c2 tests with label

--- test/cpp/suite5.cpp
+++ test/cpp/suite5.cpp
@@ -1,4 +1,12 @@
 #include "catch2/catch.hpp"
 
-TEST_CASE("spec , char") {}
-TEST_CASE("spec . char") {}
+TEST_CASE("suite with label 1", "descr [1][2]") {}
+TEST_CASE("suite with label 2", "descr [2][3]") {}
+TEST_CASE("suite with label 3", "descr[2][3]") {}
+TEST_CASE("suite with label 4", "descr [3][2]") {}
+TEST_CASE("suite with label 5", "descr [1]") {}
+TEST_CASE("suite with label 6", "descr [2]") {}
+TEST_CASE("suite with label 7", "descr [3]") {}
+TEST_CASE("suite with label 8", "descr") {}
+TEST_CASE("suite with label 9", "descr [.][3][2]") {}
+TEST_CASE("suite with label 10", "descr[hide][3][2]") {}
