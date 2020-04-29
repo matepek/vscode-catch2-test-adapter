f388f1ed68812f0d507a6c128df6ff61edb3c4a5
Mate Pek <mate.pek@gmail.com>
2020-04-29 11:12:11 +0700

    gtest parser

--- src/main.ts
+++ src/main.ts
@@ -16,32 +16,34 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
       new TestAdapterRegistrar(testHub, workspaceFolder => new TestAdapter(workspaceFolder), undefined),
     );
 
-    vscode.languages.registerDocumentLinkProvider(
-      { language: 'gtest' },
-      {
-        provideDocumentLinks: (
-          document: vscode.TextDocument,
-          token: vscode.CancellationToken, // eslint-disable-line
-        ): vscode.ProviderResult<vscode.DocumentLink[]> => {
-          const text = document.getText();
-          if (!text.startsWith('[ RUN      ]')) return null;
-          const result: vscode.DocumentLink[] = [];
-          const lines = text.split(/\r?\n/);
-          for (let i = 0; i < lines.length; ++i) {
-            const m = lines[i].match(GoogleTest.failureRe);
-            if (m) {
-              const line = Number(m[3]);
-              result.push(
-                new vscode.DocumentLink(
-                  new vscode.Range(i, 0, i, m[1].length),
-                  vscode.Uri.file(m[2]).with({ fragment: `${line}` }),
-                ),
-              );
+    context.subscriptions.push(
+      vscode.languages.registerDocumentLinkProvider(
+        { language: 'gtest' },
+        {
+          provideDocumentLinks: (
+            document: vscode.TextDocument,
+            token: vscode.CancellationToken, // eslint-disable-line
+          ): vscode.ProviderResult<vscode.DocumentLink[]> => {
+            const text = document.getText();
+            if (!text.startsWith('[ RUN      ]')) return null;
+            const result: vscode.DocumentLink[] = [];
+            const lines = text.split(/\r?\n/);
+            for (let i = 0; i < lines.length; ++i) {
+              const m = lines[i].match(GoogleTest.failureRe);
+              if (m) {
+                const line = Number(m[3]);
+                result.push(
+                  new vscode.DocumentLink(
+                    new vscode.Range(i, 0, i, m[1].length),
+                    vscode.Uri.file(m[2]).with({ fragment: `${line}` }),
+                  ),
+                );
+              }
             }
-          }
-          return result;
+            return result;
+          },
         },
-      },
+      ),
     );
   }
 }
